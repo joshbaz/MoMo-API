68fb02be884a05521783f9b09f070100df4507a8
Joshua Kimbareeba <jkimbareeba@yahoo.com>
2024-11-06 20:37:55 +0300

    PESAPAL TRANSACTIONS UPDATE

--- app/routes/pesapalTransactions.js
+++ app/routes/pesapalTransactions.js
@@ -127,6 +127,118 @@ router.post("/donate", upload.none(), generatePesaAuthTk, generateIPN_ID, async 
 
 })
 
+/** route for pesapal-status-notification only */
+router.get("/tansact_statuses", generatePesaAuthTk, async (req, res, next) => {
+    try {
+        console.log("here")
+        //  console.log("working to make connections", req.body, req.query);
+        //console.log("working", req);
+        // const { OrderTrackingId, OrderNotificationType, OrderMerchantReference } = req.query;
+        //  let url_parts = url.parse(req.url, true);
+        // let query = url_parts.query;
+        // console.log("query parts", query)
+
+        const { OrderTrackingId } = req.query;
+
+        //console.log("OrderTrackingId", OrderTrackingId)
+
+        let PESA_URL = process.env.Production_State === "production" ? process.env.PESA_LIVE_URL : process.env.PESA_Sandbox_URL
+
+        let PesaRequestLink = `${PESA_URL}/api/Transactions/GetTransactionStatus?orderTrackingId=${OrderTrackingId}`
+
+        let headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": req.pesa_access_token
+        }
+
+        let submitStatusRequest = await axios.get(PesaRequestLink, { headers: headers });
+
+        console.log("submitStatusRequest", submitStatusRequest.data)
+        if (!submitStatusRequest.data) {
+            console.log("error", submitStatusRequest.data)
+        } else {
+            const { payment_method, amount, payment_status_description, description, payment_account, currency, message } = submitStatusRequest.data;
+
+            //console.log("pay-req", payment_status_description)
+            let getTransact = await transactModel.findOne({ orderTrackingId: OrderTrackingId });
+
+            //console.log("getTransact", getTransact)
+            if (!getTransact) {
+                const error = new Error("Transaction missing");
+                error.statusCode = 404;
+                throw (error);
+            } else if (getTransact && getTransact.payment_status_description !== "Pending") {
+                res.status(200).json({
+                    payment_status_description: submitStatusRequest.data.payment_status_description,
+                    paidAmount: amount,
+                    paymentType: `PesaPal-${submitStatusRequest.data.payment_method}`,
+                    transactionId: getTransact._id,
+                    currency: submitStatusRequest.data.currency,
+
+                });
+            } else {
+                getTransact.payment_status_description = `${submitStatusRequest.data.payment_status_description}`;
+                getTransact.paidAmount = amount;
+                getTransact.status_reason = submitStatusRequest.data.description;
+                getTransact.currency = submitStatusRequest.data.currency;
+                getTransact.paymentType = `PesaPal-${submitStatusRequest.data.payment_method}`;
+
+                getTransact.save()
+                console.log("registered")
+                let template = fs.readFileSync(
+                    "./payNotify.hjs",
+                    "utf-8"
+                )
+
+                let compiledTemplate = hogan.compile(template);
+
+                let mailOptions = {
+                    from: "joshuakimbareeba@gmail.com",
+                    to: "stephaniekirathe@gmail.com",
+                    cc: "charlesaroma9@gmail.com, mymbugua@gmail.com",
+                    subject: "Alert - Payment Received",
+                    html: compiledTemplate.render({
+                        transactionType: getTransact.transactionType,
+                        paidAmount: `${amount} ${currency}`,
+                        paymentType: `PesaPal-${submitStatusRequest.data.payment_method}`,
+                        title: "Alert - Payment Received"
+                    })
+                }
+
+                transporter.sendMail(mailOptions, async (error, info) => {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        console.log('email sent: ')
+                    }
+                })
+                res.status(200).json({
+                    payment_status_description: submitStatusRequest.data.payment_status_description,
+                    paidAmount: amount,
+                    paymentType: `PesaPal-${submitStatusRequest.data.payment_method}`,
+                    transactionId: getTransact._id,
+                    currency: submitStatusRequest.data.currency,
+                    status: 200,
+                    "orderNotificationType": "IPNCHANGE",
+                    "orderTrackingId": OrderTrackingId
+                });
+            }
+
+
+        }
+
+
+    } catch (error) {
+        if (!error.statusCode) {
+            error.statusCode = 500
+            console.log("error", error)
+        }
+        next(error)
+    }
+
+})
+
 
 /** APP TRANSACTIONS */
 
@@ -323,16 +435,15 @@ router.post("/app/purchase", upload.none(), generatePesaAuthTk, generateIPN_ID, 
 
 })
 
-
 /** route for pesapal-status-notification only */
-router.get("/tansact_statuses", generatePesaAuthTk, async (req, res, next) => {
+router.get("/app/tansact_statuses", generatePesaAuthTk, async (req, res, next) => {
     try {
         console.log("here")
-      //  console.log("working to make connections", req.body, req.query);
+        //  console.log("working to make connections", req.body, req.query);
         //console.log("working", req);
         // const { OrderTrackingId, OrderNotificationType, OrderMerchantReference } = req.query;
-      //  let url_parts = url.parse(req.url, true);
-       // let query = url_parts.query;
+        //  let url_parts = url.parse(req.url, true);
+        // let query = url_parts.query;
         // console.log("query parts", query)
 
         const { OrderTrackingId } = req.query;
@@ -350,82 +461,51 @@ router.get("/tansact_statuses", generatePesaAuthTk, async (req, res, next) => {
         }
 
         let submitStatusRequest = await axios.get(PesaRequestLink, { headers: headers });
-        
+
         console.log("submitStatusRequest", submitStatusRequest.data)
         if (!submitStatusRequest.data) {
             console.log("error", submitStatusRequest.data)
         } else {
-            const { payment_method, amount, payment_status_description, description, payment_account, currency, message } = submitStatusRequest.data;
-            
-           //console.log("pay-req", payment_status_description)
-            let getTransact = await transactModel.findOne({ orderTrackingId: OrderTrackingId });
+            const { payment_method, amount, payment_status_description, description, payment_account, currency, message, merchant_reference } = submitStatusRequest.data;
 
-            //console.log("getTransact", getTransact)
-            if (!getTransact) {
-                const error = new Error("Transaction missing");
-                error.statusCode = 404;
-                throw (error);
-            } else if (getTransact && getTransact.payment_status_description !== "Pending") {
-                res.status(200).json({
-                    payment_status_description: submitStatusRequest.data.payment_status_description,
-                    paidAmount: amount,
-                    paymentType: `PesaPal-${submitStatusRequest.data.payment_method}`,
-                    transactionId: getTransact._id,
-                    currency: submitStatusRequest.data.currency,
+            const selectMessage = (shortMessage) => {
 
-                });
-            } else {
-                getTransact.payment_status_description = `${submitStatusRequest.data.payment_status_description}`;
-                getTransact.paidAmount = amount;
-                getTransact.status_reason = submitStatusRequest.data.description;
-                getTransact.currency = submitStatusRequest.data.currency;
-                getTransact.paymentType = `PesaPal-${submitStatusRequest.data.payment_method}`;
+                switch (shortMessage) {
 
-                getTransact.save()
-                console.log("registered")
-                let template = fs.readFileSync(
-                    "./payNotify.hjs",
-                    "utf-8"
-                )
-
-                let compiledTemplate = hogan.compile(template);
-
-                let mailOptions = {
-                    from: "joshuakimbareeba@gmail.com",
-                    to: "stephaniekirathe@gmail.com",
-                    cc: "charlesaroma9@gmail.com, mymbugua@gmail.com",
-                    subject: "Alert - Payment Received",
-                    html: compiledTemplate.render({
-                        transactionType: getTransact.transactionType,
-                        paidAmount: `${amount} ${currency}`,
-                        paymentType: `PesaPal-${submitStatusRequest.data.payment_method}`,
-                        title: "Alert - Payment Received"
-                    })
+                    case "failed":
+                        return "Transaction has Failed";
+                    case "completed":
+                        return "Transaction Successful";
+                    case "pending":
+                        return "Transaction Pending";
+                    case "rejected":
+                        return "Transaction Rejected";
+                    case "invalid":
+                        return "Transaction invalid"
+                    case "reversed":
+                        return "Transaction reversed"
+                    default:
+                        return null;
                 }
+            } 
 
-                transporter.sendMail(mailOptions, async (error, info) => {
-                    if (error) {
-                        console.log(error)
-                    } else {
-                        console.log('email sent: ')
-                    }
-                })
+            let paymentStatus = selectMessage(payment_status_description.toLowerCase());
+            
                 res.status(200).json({
-                    payment_status_description: submitStatusRequest.data.payment_status_description,
-                    paidAmount: amount,
-                    paymentType: `PesaPal-${submitStatusRequest.data.payment_method}`,
-                    transactionId: getTransact._id,
-                    currency: submitStatusRequest.data.currency,
-                    status: 200,
-                    "orderNotificationType": "IPNCHANGE",
-                    "orderTrackingId": OrderTrackingId
-                });
-            }
+                    payStatus: paymentStatus,
+                    status_reason: description,
+                    amount: amount,
+                    paymentType: `PesaPal-${payment_method}`,
+                    merchant_reference: merchant_reference,
+                    currency: currency,
 
-          
+                });
+           
+
+
         }
 
-       
+
     } catch (error) {
         if (!error.statusCode) {
             error.statusCode = 500
@@ -435,6 +515,9 @@ router.get("/tansact_statuses", generatePesaAuthTk, async (req, res, next) => {
     }
 
 })
+
+
+
 
 //check transaction details.
 
